d5b53076e63eb5c9ed6c8ba30524129eece2772d
Jorka Dev <shongojorka3@gmail.com>
2026-09-24 22:19:37 +0100

    Refactor code structure for improved readability and maintainability

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from news.models import News
 
 # Create your views here.
 def  index(request: HttpRequest) -> HttpResponse:
-    news_items = News.objects.all().order_by('-published_date')[:5]  # Récupère les 5 dernières actualités
+    news_items = News.objects.all().order_by('-published_date')[:3]  # Affiche uniquement les 3 dernières actualités
     return render(request, 'home/index.html', {'news_items': news_items})
